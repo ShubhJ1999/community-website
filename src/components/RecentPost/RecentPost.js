bd2445e8f5f0a677f71ec0ee25f60f03cba2bd31
Vishw Kadu <68730174+vk1511@users.noreply.github.com>
2021-01-02 12:57:23 +0530

    Update RecentPost.js

--- src/components/RecentPost/RecentPost.js
+++ src/components/RecentPost/RecentPost.js
@@ -5,18 +5,19 @@ import { BlogContext } from "../../Context/BlogContext";
 
 const RecentPost = () => {
   const { blogs } = useContext(BlogContext);
+  const letBlog = blogs[blogs.length - 1];
 
   return (
     <div className="recent-post">
-      <Link to={`/specific-blog/${blogs[0].id}`}>
-        <img src={blogs[0].thumb} alt="recent-img" className="recent-cover" />
+      <Link to={`/specific-blog/${letBlog._id}`}>
+        <img src="https://images.unsplash.com/photo-1609474710192-19d19829b72f?ixid=MXwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0NHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" alt="recent-img" className="recent-cover" />
       </Link>
       <div className="recent-post-detail">
-        <p>Technology &nbsp; Social</p>
-        <Link to={`/specific-blog/${blogs[0].id}`} className="specific-link">
-          <h1>{blogs[0].title}</h1>
+        <p>{letBlog.blogCategory}</p>
+        <Link to={`/specific-blog/${letBlog._id}`} className="specific-link">
+          <h1>{letBlog.blogTitle}</h1>
         </Link>
-        <p className="recent-post-content">{blogs[0].content}</p>
+        <p className="recent-post-content">{letBlog.blogContent}</p>
       </div>
     </div>
   );
