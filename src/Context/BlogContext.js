9cb041b2237c031d233e8a8f8089db858c4cb790
Vishw Kadu <68730174+vk1511@users.noreply.github.com>
2021-01-02 11:56:53 +0530

    Update BlogContext.js

--- src/Context/BlogContext.js
+++ src/Context/BlogContext.js
@@ -1,9 +1,23 @@
-import React, { createContext } from "react";
-import { Blog } from "../constants/BlogData";
+import React, { createContext, useEffect, useState } from "react";
+import axios from 'axios'
+//import { Blog } from "../constants/BlogData";
 
 export const BlogContext = createContext();
 
 const BlogProvider = ({ children }) => {
+  
+  const [Blog, setBlog] = useState([]);
+    useEffect(() => {
+        axios.get('https://cors-anywhere.herokuapp.com/https://communitywebsite.herokuapp.com/blogs')
+        .then(res => {
+            console.log(res)
+            setBlog(res.data)
+        })
+        .catch(err => {
+            console.log(err)
+        })
+    },[])
+  
   return (
     <BlogContext.Provider value={{ blogs: Blog }}>
       {children}
